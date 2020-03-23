31a84a5b1b6ab0dd3ad739bc36d520771f8d8335
alberto cuffaro <alberto.cuffaro@hotmail.it>
2020-03-23 12:30:56 +0100

    remove unused val

--- app/src/main/java/com/acuf5928/codingavirus/MyJsonParser.kt
+++ app/src/main/java/com/acuf5928/codingavirus/MyJsonParser.kt
@@ -31,7 +31,7 @@ class MyJsonParser {
                     val result = mutableListOf<JSONObject>()
                     try {
                         val resp = response.body!!.string()
-                        val json = JSONArray(resp).let {
+                        JSONArray(resp).let {
                             for (index in 0 until it.length()) {
                                 result.add(JSONObject(it.get(index).toString()))
                             }
